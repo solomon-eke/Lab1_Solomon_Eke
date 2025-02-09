413add35888b99bbd564b41d431712548b7448f8
Solomon Eke <ekesolomon11@gmail.com>
2025-02-09 07:42:09 -0500

    Initial Commit

---
+++ Lab1_Solomon_Eke/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lab1_Solomon_Eke
+//
+//  Created by Solomon Eke on 2025-02-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lab1_Solomon_Eke/Lab1_Solomon_EkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab1_Solomon_EkeApp.swift
+//  Lab1_Solomon_Eke
+//
+//  Created by Solomon Eke on 2025-02-09.
+//
+
+import SwiftUI
+
+@main
+struct Lab1_Solomon_EkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
